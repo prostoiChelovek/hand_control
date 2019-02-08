06bb4a4b65d0dce99f620bd56f342f5c3577def4
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2019-02-08 12:08:16 +0500

    add ROI

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int release_maxDistChange = 50;
 string faceCascadePath = "/data/haarcascade_frontalface_alt.xml";
 Size minFaceSize = Size(70, 70);
 
-void adjustColorRanges(string wName) {
+void adjustColorRanges(const string &wName) {
     namedWindow(wName);
     auto onTrackbarActivity = [](int val, void *data) {
         double &vNum = *(static_cast<double *>(data));
@@ -46,6 +46,41 @@ void adjustColorRanges(string wName) {
     createTrackbar("YMax", wName, &YMaxVal, 255, onTrackbarActivity, &upper.val[2]);
 }
 
+bool should_controlMouse = false;
+bool should_click = false;
+bool should_flipVert = true;
+bool should_flipHor = true;
+bool should_adjustBox = false;
+
+void flipImg(Mat &img) {
+    if (should_flipVert)
+        flip(img, img, 0);
+    if (should_flipHor)
+        flip(img, img, 1);
+}
+
+Rect box;
+bool mbtnDown = false;
+
+static void adjustBox_cb(int event, int x, int y, int, void *) {
+    if (!should_adjustBox) return;
+    switch (event) {
+        case CV_EVENT_LBUTTONDOWN:
+            box.x = x;
+            box.y = y;
+            mbtnDown = true;
+            break;
+        case CV_EVENT_LBUTTONUP:
+            mbtnDown = false;
+            break;
+        case CV_EVENT_MOUSEMOVE:
+            if (!mbtnDown) break;
+            box.width = x;
+            box.height = y;
+            break;
+    }
+}
+
 int main() {
     VideoCapture cap(0);
     if (!cap.isOpened()) {
@@ -58,6 +93,9 @@ int main() {
     HandDetector hd;
 
     hd.shouldCheckAngles = false;
+    hd.processNoiseCov = Scalar::all(7);
+    hd.measurementNoiseCov = Scalar::all(6e-2);
+    hd.errorCovPost = Scalar::all(.3);
 
     CascadeClassifier faceCascade;
     faceCascade.load(faceCascadePath);
@@ -65,15 +103,19 @@ int main() {
         cerr << "Could not load face cascade " << faceCascadePath << endl;
 
     Mat frame, img, img2, mask, bg, imgYCrCb;
-    cap >> bg;
 
     adjustColorRanges("Adjust ranges");
+    namedWindow("img");
+    setMouseCallback("img", adjustBox_cb);
 
     int lastDist = 0;
 
+    cap >> bg;
+    flipImg(bg);
     while (cap.isOpened()) {
         char key;
         cap >> frame;
+        flipImg(frame);
         frame.copyTo(img);
         frame.copyTo(img2);
 
@@ -101,26 +143,40 @@ int main() {
         hd.getHigherFingers();
         hd.getFarthestFingers();
 
+        rectangle(img2, box, Scalar(0, 255, 0));
         hd.drawHands(img2, Scalar(255, 0, 100), 2);
 
         if (!hd.hands.empty()) {
-            Hand &h = hd.hands[0];
-            Finger &f = h.higherFinger;
-            Point &p = f.ptStart;
-            SysInter::moveMouse(p.x * (SysInter::winWidth / img.cols),
-                                p.y * (SysInter::winHeight / img.rows));
+            Hand *h;
+            Point *p;
+            bool found = false;
+            for (Hand &hnd : hd.hands) {
+                Point &pt = hnd.higherFinger.ptStart;
+                if (box.contains(pt)) {
+                    h = &hnd;
+                    p = &pt;
+                    found = true;
+                }
+            }
 
-            int dist = (h.border.x + h.border.width) - p.x;
-            int distChange = lastDist - dist;
-            if (distChange >= press_minDistChange && distChange <= press_maxDistChange) {
-                SysInter::mouseClick(Button1, true);
-                cout << distChange << " press" << endl;
+            if (should_controlMouse && found) {
+                SysInter::moveMouse((p->x - box.x) * (SysInter::winWidth / box.width),
+                                    (p->y - box.y) * (SysInter::winHeight / box.height));
             }
-            if (-distChange >= release_minDistChange && -distChange <= release_maxDistChange) {
-                SysInter::mouseClick(Button1, false);
-                cout << distChange << " release" << endl;
+
+            if (should_click && found) {
+                int dist = (h->border.x + h->border.width) - p->x;
+                int distChange = lastDist - dist;
+                if (distChange >= press_minDistChange && distChange <= press_maxDistChange) {
+                    SysInter::mouseClick(Button1, true);
+                    cout << distChange << " press" << endl;
+                }
+                if (-distChange >= release_minDistChange && -distChange <= release_maxDistChange) {
+                    SysInter::mouseClick(Button1, false);
+                    cout << distChange << " release" << endl;
+                }
+                lastDist = dist;
             }
-            lastDist = dist;
         }
 
         imshow("img", img2);
@@ -134,9 +190,23 @@ int main() {
                     cout << "Exit" << endl;
                     return EXIT_SUCCESS;
                 case 'b':
-                    cap >> bg;
+                    frame.copyTo(bg);
                     cout << "Background captured" << endl;
                     break;
+                case 'm':
+                    should_controlMouse = !should_controlMouse;
+                    break;
+                case 'c':
+                    should_click = !should_click;
+                    break;
+                case 'v':
+                    should_flipHor = !should_flipVert;
+                    break;
+                case 'h':
+                    should_flipHor = !should_flipHor;
+                    break;
+                case 'o':
+                    should_adjustBox = !should_adjustBox;
                 default:
                     cout << "Key presed: " << key << endl;
                     break;
